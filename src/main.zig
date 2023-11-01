a012c78327dbad5be7697fe0f49033ca1411085b
Blake Vollbrecht <blakevollbrecht@gmail.com>
2023-11-01 15:33:03 -0400

    Added running time to Zig solutions

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,13 @@
-const solution = @import("solutions/13-2.zig");
+const std = @import("std");
+
+const solution = @import("solutions/12-1.zig");
 
 pub fn main() !void {
+    const start = std.time.nanoTimestamp();
+
     try solution.solve();
+
+    const end = std.time.nanoTimestamp();
+    const time_ms = (@as(f64, @floatFromInt(end)) - @as(f64, @floatFromInt(start))) / 1000000;
+    std.debug.print("Time: {d}ms", .{time_ms});
 }
